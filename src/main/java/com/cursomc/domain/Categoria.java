a9a73cceaef201f209e21f2c67e3e4b808dac285
Igor Oliveira <yggor_2008@hotmail.com>
2018-10-01 12:16:15 -0300

    Bd h2 e criacao automatica do database

--- src/main/java/com/cursomc/domain/Categoria.java
+++ src/main/java/com/cursomc/domain/Categoria.java
@@ -2,33 +2,47 @@ package com.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
-	
+
 	public Categoria() {
 		super();
 	}
+
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -36,6 +50,7 @@ public class Categoria implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -52,7 +67,5 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
